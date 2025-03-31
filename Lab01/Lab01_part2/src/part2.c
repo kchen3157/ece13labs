83ee764c968ac04eee0996ad0927337bff5baabf
kyevchen <kyevchen@ucsc.edu>
2025-03-31 13:15:01 -0700

    lab 1 part 2 init

---
+++ Lab01/Lab01_part2/src/part2.c
@@ -0,0 +1,57 @@
+/**
+ * @file    part2.c
+ *
+ * Convert temperatures from Fahrenheit to Celsius and Kelvin.
+ *
+ * @author  Roronoa Zoro (rorozoro3ucsc.edu)
+ * 
+ * @date    January 1, 1970
+ */
+// Standard libraries.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Course libraries.
+#include <BOARD.h>
+
+
+/**
+ * There can be only one!
+ */
+int main(void) {
+    BOARD_Init();
+    while(1)
+    {
+        /***********************************************************************
+         * Your code goes in between this comment and the following one with   *
+         * asterisks.                                                          *
+         **********************************************************************/
+        // Declare variables.
+        float fahr, celsius;
+        int lower, upper, step;
+
+        // Initialize variables.
+        lower = 0;
+        upper = 300;
+        step = 20;
+        fahr = lower;
+
+        // Print out the table.
+        while (fahr <= upper) 
+        {
+            celsius = (5.0 / 9.0) * (fahr - 32.0);
+            printf("%7.1f %04.1f\r\n", (double) fahr, (double) celsius);
+            fahr = fahr + step;
+        }
+
+        /***********************************************************************
+         * Your code goes in between this comment and the preceding one with   *
+         * asterisks.                                                          *
+         **********************************************************************/
+        HAL_Delay(3333);
+    }
+    BOARD_End();
+    // Returning from main() is bad form in embedded environments.
+    // So we sit and spin.
+    while(1);
+}
