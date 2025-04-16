f4a6eb023e0f8d1874a84bf97b47cc4d9a0d85c7
kyevchen <kyevchen@ucsc.edu>
2025-04-16 15:10:30 -0700

    implement stack functions

--- Lab05/stack.c
+++ Lab05/stack.c
@@ -5,3 +5,53 @@
  *
  * @date 2025-04-16
  */
+
+
+ #include "stack.h"
+
+ void StackInit(struct Stack *stack)
+ {
+    stack->currentItemIndex = -1;
+    stack->initialized = TRUE;
+ }
+
+ int StackPush(struct Stack *stack, double value)
+ {
+    if (stack->currentItemIndex >= (STACK_SIZE - 1) || !(stack->initialized))
+    {
+        return STANDARD_ERROR;
+    }
+    stack->currentItemIndex++;
+    stack->stackItems[stack->currentItemIndex] = value;
+    return SUCCESS;
+ }
+
+ int StackPop(struct Stack *stack, double *value)
+ {
+    if (stack->currentItemIndex <= -1 || !(stack->initialized))
+    {
+        return STANDARD_ERROR;
+    }
+    *value = stack->stackItems[stack->currentItemIndex];
+    stack->currentItemIndex--;
+    return SUCCESS;
+ }
+
+ int StackIsEmpty(const struct Stack *stack)
+ {
+    return (stack->initialized && stack->currentItemIndex <= -1) ? TRUE : FALSE;
+ }
+
+ int StackIsFull(const struct Stack *stack)
+ {
+    return (stack->initialized && stack->currentItemIndex >= (STACK_SIZE - 1)) ? TRUE : FALSE;
+ }
+
+ int StackGetSize(const struct Stack *stack)
+ {
+    if (!(stack->initialized))
+    {
+        return SIZE_ERROR;
+    }
+    return (stack->currentItemIndex + 1);
+ }
