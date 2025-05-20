e368caee33edf1c9a19de98d774e64b3f2329044
kyevchen <kyevchen@ucsc.edu>
2025-05-20 13:27:20 -0700

    ui function

---
+++ Lab08/src/Buttons.c
@@ -0,0 +1,143 @@
+/**
+ * @file        Buttons.c
+ *
+ * @author      Kyle Chen
+ *
+ * @date        2025-05-06
+ */
+
+// **** Include libraries here ****
+// Standard libraries.
+
+// Course libraries.
+#include "Buttons.h"
+
+// **** Set macros and preprocessor directives ****
+
+// **** Declare any datatypes here ****
+
+// **** Define global, module-level, or external variables here ****
+static volatile uint8_t debounceCounter[4] = {0, 0, 0, 0};
+static volatile uint8_t savedButtonState = 0x00;
+
+// **** Declare function prototypes ****
+
+void Buttons_Init(void)
+{
+    return;
+}
+
+uint8_t Buttons_End(void)
+{
+    return 0;
+}
+
+/**
+ * ButtonsCheckEvents function checks the current button states and returns
+ * any events that have occured since its last call.  This function should be
+ * called repeatedly in a Timer ISR, though it can be called in main() during
+ * testing.
+ *
+ * In normal use, this function should only be used after ButtonsInit().
+ *
+ * This function should assume that the buttons start in an off state with value
+ * 0. Therefore if no buttons are pressed when ButtonsCheckEvents() is first
+ * called, BUTTONS_EVENT_NONE should be returned.
+ *
+ * @return  Each bit of the return value corresponds to one ButtonEvent flag,
+ *          as described in Buttons.h.  If no events are detected,
+ *          BUTTONS_EVENT_NONE is returned.
+ *
+ * Note that more than one event can occur simultaneously, though this
+ * situation is rare. To handle this, the output should be a bitwise OR of
+ * all applicable event flags.  For example, if button 1 was released at the
+ * same time that button 2 was pressed, this function should return
+ * (BUTTON_EVENT_1UP | BUTTON_EVENT_2DOWN).
+ */
+uint8_t Buttons_CheckEvents(void)
+{
+    uint8_t buttonEvents = BUTTON_EVENT_NONE;
+
+#ifdef STM32F4
+    enum ButtonStateFlags currentButtonState = ~BUTTON_STATES();
+
+    //********** Debounce filter for BTN_1 **********
+    if ((currentButtonState & BUTTON_STATE_1) != (savedButtonState & BUTTON_STATE_1))
+    {
+        if ((debounceCounter[0] < BUTTONS_DEBOUNCE_PERIOD))
+        {
+            debounceCounter[0]++;
+        }
+        else
+        {
+            buttonEvents |= (currentButtonState & BUTTON_STATE_1) ? BUTTON_EVENT_1DOWN : BUTTON_EVENT_1UP;
+            savedButtonState ^= BUTTON_STATE_1;
+            debounceCounter[0] = 0;
+        }
+    }
+    else
+    {
+        debounceCounter[0] = 0;
+    }
+
+    //********** Debounce filter for BTN_2 **********
+    if ((currentButtonState & BUTTON_STATE_2) != (savedButtonState & BUTTON_STATE_2))
+    {
+        if ((debounceCounter[1] < BUTTONS_DEBOUNCE_PERIOD))
+        {
+            debounceCounter[1]++;
+        }
+        else
+        {
+            buttonEvents |= (currentButtonState & BUTTON_STATE_2) ? BUTTON_EVENT_2DOWN : BUTTON_EVENT_2UP;
+            savedButtonState ^= BUTTON_STATE_2;
+            debounceCounter[1] = 0;
+        }
+    }
+    else
+    {
+        debounceCounter[1] = 0;
+    }
+
+    //********** Debounce filter for BTN_3 **********
+    if ((currentButtonState & BUTTON_STATE_3) != (savedButtonState & BUTTON_STATE_3))
+    {
+        if ((debounceCounter[2] < BUTTONS_DEBOUNCE_PERIOD))
+        {
+            debounceCounter[2]++;
+        }
+        else
+        {
+            buttonEvents |= (currentButtonState & BUTTON_STATE_3) ? BUTTON_EVENT_3DOWN : BUTTON_EVENT_3UP;
+            savedButtonState ^= BUTTON_STATE_3;
+            debounceCounter[2] = 0;
+        }
+    }
+    else
+    {
+        debounceCounter[2] = 0;
+    }
+
+    //********** Debounce filter for BTN_4 **********
+    if ((currentButtonState & BUTTON_STATE_4) != (savedButtonState & BUTTON_STATE_4))
+    {
+        if ((debounceCounter[3] < BUTTONS_DEBOUNCE_PERIOD))
+        {
+            debounceCounter[3]++;
+        }
+        else
+        {
+            buttonEvents |= (currentButtonState & BUTTON_STATE_4) ? BUTTON_EVENT_4DOWN : BUTTON_EVENT_4UP;
+            savedButtonState ^= BUTTON_STATE_4;
+            debounceCounter[3] = 0;
+        }
+    }
+    else
+    {
+        debounceCounter[3] = 0;
+    }
+
+#endif /*  STM32F4 */
+
+    return buttonEvents;
+}
--- Lab08/src/Lab08_main.c
+++ Lab08/src/Lab08_main.c
@@ -1,9 +1,9 @@
 /**
- * @file    Lab08_main_template.c
+ * @file        Lab08_main.c
  *
- * @author
+ * @author      Kyle Chen
  *
- * @date
+ * @date        05-19-2025
  */
 // **** Include libraries here ****
 // Standard libraries.
@@ -18,6 +18,14 @@
 #include <Timers.h>
 
 // **** Set any macros or preprocessor directives here ****
+#define BAKE_UI     "BAKE"
+#define TOAST_UI    "TOAST"
+#define BROIL_UI    "BROIL"
+#define ERROR_UI    "ERROR"
+#define SEL_UI      '>'
+#define NOT_SEL_UI  ' '
+
+#define DEFAULT_COOK_MODE   BAKE
 
 // **** Set any local typedefs here ****
 typedef enum
@@ -28,10 +36,28 @@ typedef enum
     RESET_PENDING
 } OvenState;
 
+typedef enum
+{
+    BAKE,
+    TOAST,
+    BROIL
+} OvenCookMode;
+
+typedef enum
+{
+    TIME,
+    TEMP
+} OvenSettingSelect;
+
 typedef struct
 {
-    OvenState state;
-    // add more members to this struct
+    OvenState           state;                  // Main toaster state
+    OvenCookMode        cook_mode;              // Which cook mode is selected
+    OvenSettingSelect   setting_select;         // Which setting is selected for configuration
+    uint16_t            setting_cook_time;      // The set cook time in seconds
+    uint16_t            setting_temperature;    // The set cook temperature in Fahrenheit (Range: 300-555)
+    uint16_t            cook_time_left;         // Cook time left in seconds (Range: 1-256)
+    uint16_t            button_hold_time;       // Time button was held in milliseconds
 } OvenData;
 
 // **** Declare any datatypes here ****
@@ -45,6 +71,83 @@ static volatile OvenData oven;
 void updateOvenOLED(void)
 {
     // Update OLED here.
+    char oled_buffer[100];
+    char* cook_mode_ui;
+    char time_sel_ui;
+    char temp_sel_ui;
+    uint8_t time_min;
+    uint8_t time_sec;
+
+    if (oven.setting_select == TIME)
+    {
+        time_sel_ui = SEL_UI;
+        temp_sel_ui = NOT_SEL_UI;
+    }
+    else
+    {
+        time_sel_ui = NOT_SEL_UI;
+        temp_sel_ui = SEL_UI;
+    }
+
+    // Oven cook mode display
+    if (oven.cook_mode == BAKE)
+    {
+        cook_mode_ui = BAKE_UI;
+    }
+    else if (oven.cook_mode == BROIL)
+    {
+        cook_mode_ui = BROIL_UI;
+    }
+    else if (oven.cook_mode == TOAST)
+    {
+        cook_mode_ui = TOAST_UI;
+    }
+    else
+    {
+        cook_mode_ui = ERROR_UI;
+    }
+
+    // Oven time
+    if (oven.state == COOKING)
+    {
+        time_min = (uint8_t) (oven.cook_time_left / 60);
+        time_sec = (uint8_t) (oven.cook_time_left % 60);
+    }
+    else
+    {
+        time_min = (uint8_t) (oven.setting_cook_time / 60);
+        time_sec = (uint8_t) (oven.setting_cook_time % 60);
+    }
+
+    // Load ui data into buffer
+    if (oven.state == COOKING && oven.cook_mode == BAKE)
+    {
+        sprintf(oled_buffer, "|\x01\x01\x01\x01\x01| MODE: %s\n|     | %cTIME: %d:%02d\n|     | %cTEMP: %d\xF8\n|\x03\x03\x03\x03\x03|", 
+            cook_mode_ui, time_sel_ui, time_min, time_sec, temp_sel_ui, oven.setting_temperature);
+    }
+    else if (oven.state == COOKING && oven.cook_mode == BROIL)
+    {
+        sprintf(oled_buffer, "|\x01\x01\x01\x01\x01| MODE: %s\n|     | %cTIME: %d:%02d\n|     | %cTEMP: %d\xF8\n|\x04\x04\x04\x04\x04|", 
+            cook_mode_ui, time_sel_ui, time_min, time_sec, temp_sel_ui, oven.setting_temperature);
+    }
+    else if (oven.state == COOKING && oven.cook_mode == TOAST)
+    {
+        sprintf(oled_buffer, "|\x02\x02\x02\x02\x02| MODE: %s\n|     | TIME: %d:%02d\n|     |\n|\x03\x03\x03\x03\x03|", 
+            cook_mode_ui, time_min, time_sec);
+    }
+    else if (oven.cook_mode == TOAST)
+    {
+        sprintf(oled_buffer, "|\x02\x02\x02\x02\x02| MODE: %s\n|     | TIME: %d:%02d\n|     |\n|\x04\x04\x04\x04\x04|", 
+            cook_mode_ui, time_min, time_sec);
+    }
+    else
+    {
+        sprintf(oled_buffer, "|\x02\x02\x02\x02\x02| MODE: %s\n|     | %cTIME: %d:%02d\n|     | %cTEMP: %d\xF8\n|\x04\x04\x04\x04\x04|", 
+            cook_mode_ui, time_sel_ui, time_min, time_sec, temp_sel_ui, oven.setting_temperature);
+    }
+
+    OLED_DrawString(oled_buffer);
+    OLED_Update();
 }
 
 /* This function will execute your state machine.
@@ -53,6 +156,20 @@ void updateOvenOLED(void)
 void runOvenSM(void)
 {
     // Write your SM logic here.
+
+    if (oven.state == SETUP)
+        {
+            if (Buttons_CheckEvents() == BUTTON_EVENT_3DOWN)
+            {
+                oven.state = SELECTOR_CHANGE_PENDING;
+            }
+        }
+
+
+        if (oven.state == SELECTOR_CHANGE_PENDING)
+        {
+            
+        }
 }
 
 int main()
@@ -71,6 +188,13 @@ int main()
         __DATE__);
 
     // Initialize state machine (and anything else you need to init) here.
+    oven.state = SETUP;
+    oven.cook_mode = BAKE;
+    oven.button_hold_time = 0;
+    oven.setting_cook_time = 1;
+    oven.cook_time_left = 1;
+    oven.setting_temperature = 350;
+    
 
     while (1)
     {
@@ -78,7 +202,8 @@ int main()
         // check for events
         // on event, run runOvenSM()
         // clear event flags
-    };
+        updateOvenOLED();
+    }
 
     BOARD_End();
     while(1);
