e5d67d40d34ad50c5d6d800b7e81826e3ad80fec
kyevchen <kyevchen@ucsc.edu>
2025-06-06 15:23:17 -0700

    add negotiation test function

---
+++ Lab10/src/NegotiationTest.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "Negotiation.h"
+
+int main(void)
+{
+
+    //******** NegotiationHash ********
+    printf("Testing NegotiationHash(3)...\n");
+    printf("\tExpected: 9\n");
+    printf("\tGot: %u\n", (uint16_t) NegotiationHash((NegotiationData) 3));
+
+    printf("Testing NegotiationHash(12345)...\n");
+    printf("\tExpected: 43182\n");
+    printf("\tGot: %u\n", (uint16_t) NegotiationHash((NegotiationData) 12345));
+
+    //******** NegotiationVerify ********
+    printf("Testing NegotiationVerify(3, 9)...\n");
+    printf("\tExpected: 1\n");
+    printf("\tGot: %d\n", NegotiationVerify(3, 9));
+    printf("Testing NegotiationVerify(12345, 43182)...\n");
+    printf("\tExpected: 1\n");
+    printf("\tGot: %d\n", NegotiationVerify(12345, 43182));
+    printf("Testing NegotiationVerify(12345, 13742)...\n");
+    printf("\tExpected: 0\n");
+    printf("\tGot: %d\n", NegotiationVerify(12345, 13742));
+
+    //******** NegotiationCoinFlip ********
+    printf("Testing NegotiateCoinFlip(0b00001011, 0b01100000)...\n");
+    printf("\tGot: %u\n", NegotiateCoinFlip((NegotiationData) 0b00001011, (NegotiationData) 0b01100000));
+
+    return 0;
+}
