49be962c4ce5191d982670fa19848d353cdb4c1c
kyevchen <kyevchen@ucsc.edu>
2025-06-06 15:23:17 -0700

    add more tests for field module and organize fieldtest print output

--- Lab10/src/FieldTest.c
+++ Lab10/src/FieldTest.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "Field.h"
 #include "BOARD.h"
@@ -12,6 +14,7 @@
 int main(void)
 {
     BOARD_Init();
+
     Field ownfield_uut, oppfield_uut;
     GuessData guess_uut;
     
@@ -19,15 +22,22 @@ int main(void)
     
 
     //******** FieldInit/FieldPrint_UART ********
+    printf("FieldInit/FieldPrint_UART Test\n");
+
     FieldInit(&ownfield_uut, &oppfield_uut);
+    printf("Verify following field is empty:\n");
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
+    printf("\n\n");
 
     //******** FieldAddBoat ********
+    printf("FieldAddBoat Test\n");
+    
     // Initial Boat placement
     if (FieldAddBoat(&ownfield_uut, 0, 0, FIELD_DIR_EAST, FIELD_BOAT_TYPE_HUGE)) {FAIL("bp1")}
     if (FieldAddBoat(&ownfield_uut, 2, 0, FIELD_DIR_EAST, FIELD_BOAT_TYPE_MEDIUM)) {FAIL("bp3")}
     if (FieldAddBoat(&ownfield_uut, 3, 0, FIELD_DIR_EAST, FIELD_BOAT_TYPE_SMALL)) {FAIL("bp4")}
 
+    printf("Verify three boats, then four boats placed:\n");
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
 
     // Try placing boats on other boats
@@ -53,46 +63,57 @@ int main(void)
 
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
 
+    printf("\n\n");
+
     //******** FieldRegisterEnemyAttack ********
+    printf("FieldRegisterEnemyAttack Test\n");
+    printf("There should be no difference between the two field prints, 7 hit/misses in total\n");
 
     // Do valid attacks
 
+    // Do an enemy hit
     guess_uut.row = 0;
     guess_uut.col = 0;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_HUGE_BOAT) {FAIL("bp16p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_HIT) {FAIL("bp16")}
     if (guess_uut.result != RESULT_HIT) {FAIL("bp16a")}
 
+    // Do an enemy miss
     guess_uut.row = 1;
     guess_uut.col = 7;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_EMPTY) {FAIL("bp17p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_MISS) {FAIL("bp17")}
     if (guess_uut.result != RESULT_MISS) {FAIL("bp17a")}
 
+    // Do an enemy hit
     guess_uut.row = 3;
     guess_uut.col = 0;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_SMALL_BOAT) {FAIL("bp18p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_HIT) {FAIL("bp18")}
     if (guess_uut.result != RESULT_HIT) {FAIL("bp18a")}
 
+    // Do an enemy hit
     guess_uut.row = 3;
     guess_uut.col = 1;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_SMALL_BOAT) {FAIL("bp19p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_HIT) {FAIL("bp19")}
     if (guess_uut.result != RESULT_HIT) {FAIL("bp19a")}
 
+    // Do an enemy hit
     guess_uut.row = 3;
     guess_uut.col = 2;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_SMALL_BOAT) {FAIL("bp20p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_HIT) {FAIL("bp20")}
     if (guess_uut.result != RESULT_SMALL_BOAT_SUNK) {FAIL("bp20a")}
 
+    // Do an enemy miss
     guess_uut.row = 5;
     guess_uut.col = 2;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_EMPTY) {FAIL("bp21p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_MISS) {FAIL("bp21")}
     if (guess_uut.result != RESULT_MISS) {FAIL("bp21a")}
 
+    // Do an enemy miss
     guess_uut.row = 4;
     guess_uut.col = 6;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_EMPTY) {FAIL("bp22p")}
@@ -103,42 +124,77 @@ int main(void)
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
 
     // Now do invalid attacks
+
+    // Out of bounds
     guess_uut.row = -1;
     guess_uut.col = -8;
     if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_INVALID) {FAIL("bp23p")}
     if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_INVALID) {FAIL("bp23")}
     if (guess_uut.result != RESULT_MISS) {FAIL("bp23a")}
 
+    // Enemy attack on previously attacked missed square 
+    guess_uut.row = 4;
+    guess_uut.col = 6;
+    if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_MISS) {FAIL("bp24p")}
+    if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_MISS) {FAIL("bp24")}
+    if (guess_uut.result != RESULT_MISS) {FAIL("bp24a")}
+
+
+    // Enemy attack on previously attacked hit square 
+    guess_uut.row = 3;
+    guess_uut.col = 2;
+    if (FieldRegisterEnemyAttack(&ownfield_uut, &guess_uut) != FIELD_SQUARE_HIT) {FAIL("bp25p")}
+    if (FieldGetSquareStatus(&ownfield_uut, guess_uut.row, guess_uut.col) != FIELD_SQUARE_HIT) {FAIL("bp25")}
+    if (guess_uut.result != RESULT_HIT) {FAIL("bp25a")}
+
+    FieldPrint_UART(&ownfield_uut, &oppfield_uut);
+
+    printf("\n\n");
+
+
     //******** FieldUpdateKnowledge ********
+    printf("FieldUpdateKnowledge Test\n");
+
     guess_uut.row = 0;
     guess_uut.col = 0;
     guess_uut.result = RESULT_HIT;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp26p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_HIT) {FAIL("bp26")}
 
     guess_uut.row = 1;
     guess_uut.col = 0;
     guess_uut.result = RESULT_MISS;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp27p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_MISS) {FAIL("bp27")}
 
     guess_uut.row = 2;
     guess_uut.col = 0;
     guess_uut.result = RESULT_HUGE_BOAT_SUNK;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp28p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_HIT) {FAIL("bp28a")}
+    if (oppfield_uut.hugeBoatLives != 0) {FAIL("bp28b")}
+
 
     guess_uut.row = 3;
     guess_uut.col = 0;
     guess_uut.result = RESULT_LARGE_BOAT_SUNK;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp29p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_HIT) {FAIL("bp29a")}
+    if (oppfield_uut.largeBoatLives != 0) {FAIL("bp29b")}
 
     guess_uut.row = 4;
     guess_uut.col = 0;
     guess_uut.result = RESULT_MEDIUM_BOAT_SUNK;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp30p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_HIT) {FAIL("bp30a")}
+    if (oppfield_uut.mediumBoatLives != 0) {FAIL("bp30b")}
 
     guess_uut.row = 5;
     guess_uut.col = 0;
     guess_uut.result = RESULT_SMALL_BOAT_SUNK;
-    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    if (FieldUpdateKnowledge(&oppfield_uut, &guess_uut) != FIELD_SQUARE_UNKNOWN) {FAIL("bp31p")}
+    if (oppfield_uut.grid[guess_uut.row][guess_uut.col] != FIELD_SQUARE_HIT) {FAIL("bp31a")}
+    if (oppfield_uut.smallBoatLives != 0) {FAIL("bp31b")}
 
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
 
@@ -148,19 +204,44 @@ int main(void)
     
     //******** FieldGetBoatStates ********
     //******** FieldAIPlaceAllBoats ********
+
+    printf("FieldAIPlaceAllBoats Run %u", i + 1);
     FieldInit(&ownfield_uut, &oppfield_uut);
     FieldAIPlaceAllBoats(&ownfield_uut);
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
+
     //******** FieldAIDecideGuess ********
-    
+
     guess_uut = FieldAIDecideGuess(&oppfield_uut);
     guess_uut.result = RESULT_HIT;
     FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
 
-    FieldPrint_UART(&ownfield_uut, &oppfield_uut);
-
     guess_uut = FieldAIDecideGuess(&oppfield_uut);
     guess_uut.result = RESULT_HIT;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_MISS;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_MISS;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_HUGE_BOAT_SUNK;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_LARGE_BOAT_SUNK;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_MEDIUM_BOAT_SUNK;
+    FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
+    
+    guess_uut = FieldAIDecideGuess(&oppfield_uut);
+    guess_uut.result = RESULT_SMALL_BOAT_SUNK;
     FieldUpdateKnowledge(&oppfield_uut, &guess_uut);
 
     FieldPrint_UART(&ownfield_uut, &oppfield_uut);
